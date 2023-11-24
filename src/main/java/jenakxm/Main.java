7c95918ce8a0ab0aedcd7ec2e9760ab32e8dc8b5
jenakxm <jenakxm@gmail.com>
2023-11-24 11:34:28 +0900

    main default

--- src/main/java/jenakxm/Main.java
+++ src/main/java/jenakxm/Main.java
@@ -3,11 +3,11 @@ package jenakxm;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        FindPrime_1929 findPrime1929 = new FindPrime_1929();
-        findPrime1929.prime();
+
+
     }
 }
